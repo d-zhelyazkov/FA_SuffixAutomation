9959e412a3ffc4c28703d790b34411140565c200
D Zhelyazkov LT <d.zhelyazkov.7331@gmail.com>
2017-02-12 22:29:16 +0200

    Refactoring.

--- runATest.sh
+++ runATest.sh
@@ -1,3 +1,9 @@
-fileInfo=$(du -sh $1)
+fileSize=$(wc -c <$1)
+if [ $fileSize -le 2000 ]; then
+    fileInfo="$fileSize B\t$1"
+else
+	fileInfo=$(du -sh $1)
+fi
+
 echo -e "\n\n\nTesting with input file |$fileInfo|\n"
 time sh 25381.sh $1
